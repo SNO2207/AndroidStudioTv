b7ebc3be88b74db91848afb447ba9d439bbfc597
SNO2207 <leviladden@gmail.com>
2024-03-04 10:57:14 -0600

    Added pause functionality and stop button

--- app/src/main/java/com/example/androidstudiotv/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiotv/MainActivity.kt
@@ -13,10 +13,15 @@ import android.media.MediaPlayer
 class MainActivity : AppCompatActivity() {
     private lateinit var inputTime: EditText
     private lateinit var startTimerButton: Button
+    private lateinit var stopTimerButton: Button
     private var countDownTimer: CountDownTimer? = null
     private lateinit var timerTextView: TextView
     private lateinit var secondText: EditText
     private lateinit var minuteText: EditText
+    private var timeInMillis: Long = 0
+    private var storedTimeInMillis: Long = 0
+    private var timeInSeconds: Long = 0
+    private var isPaused: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,16 +33,52 @@ class MainActivity : AppCompatActivity() {
         timerTextView = findViewById(R.id.timerTextView)
 
         startTimerButton.setOnClickListener {
+            if(timeInMillis != 0L) {
+                if(isPaused) {
+                    resumeTimer()
+                    isPaused = false
+                    startTimerButton.text = "Pause"
+                    return@setOnClickListener
+                }
+                else {
+                    pauseTimer()
+                    isPaused = true
+                    startTimerButton.text = "Resume"
+                    return@setOnClickListener
+                }
+            }
             var minutes = 0L
-            if(minuteText.text.toString().isNotEmpty()) {
-                minutes = minuteText.text.toString().toLong()
+            if(minuteText.text.toString().isNotEmpty() && minuteText.text.toString().length <= 7) {
+                if(checkNumberInput(minuteText.text.toString()))
+                    minutes = minuteText.text.toString().toLong()
+                else {
+                    inputErrorAlert()
+                    return@setOnClickListener
+                }
             }
             var seconds = 0L
-            if(secondText.text.toString().isNotEmpty()) {
-                seconds = secondText.text.toString().toLong()
+            if(secondText.text.toString().isNotEmpty() && secondText.text.toString().length <= 7) {
+                if(checkNumberInput(secondText.text.toString()))
+                    seconds = secondText.text.toString().toLong()
+                else {
+                    inputErrorAlert()
+                    return@setOnClickListener
+                }
+            }
+            if(minutes == 0L && seconds == 0L) {
+                inputErrorAlert()
+                return@setOnClickListener
             }
             val timeInSeconds = (minutes * 60) + seconds
-            startTimer(timeInSeconds * 1000) // Convert to milliseconds
+            timeInMillis = timeInSeconds * 1000
+            startTimer(timeInMillis) // Convert to milliseconds
+            startTimerButton.text = "Pause"
+        }
+
+        stopTimerButton = findViewById(R.id.stopTimerButton)
+        stopTimerButton.setOnClickListener {
+            stopTimer()
+            startTimerButton.text = "Start"
         }
     }
 
@@ -46,7 +87,8 @@ class MainActivity : AppCompatActivity() {
         countDownTimer = object : CountDownTimer(timeInMillis, 1) {
             override fun onTick(millisUntilFinished: Long) {
                 // Convert to seconds
-                val timeInSeconds = round(millisUntilFinished / 1000.0).toInt()
+                storedTimeInMillis = millisUntilFinished
+                timeInSeconds = round(millisUntilFinished / 1000.0).toLong()
                 // Display time in MM:SS format
                 timerTextView.text = String.format("%02d:%02d", timeInSeconds / 60, timeInSeconds % 60)
             }
@@ -57,6 +99,36 @@ class MainActivity : AppCompatActivity() {
                 showTimerFinishedAlert()
             }
         }.start()
+    }
+
+    private fun stopTimer() {
+        countDownTimer?.cancel()
+        timeInMillis = 0
+        timerTextView.text = "Timer Stopped!"
+    }
+
+    private fun pauseTimer() {
+        countDownTimer?.cancel()
+    }
+
+    private fun resumeTimer() {
+        startTimer(storedTimeInMillis)
+    }
+
+    private fun inputErrorAlert() {
+        val alertDialogBuilder = AlertDialog.Builder(this@MainActivity)
+        alertDialogBuilder.setTitle("Input Error")
+        alertDialogBuilder.setMessage("Please enter a valid number.")
+        alertDialogBuilder.setPositiveButton("OK") { dialog, which ->
+            // Do nothing.
+        }
+        // Optionally, add a negative button or other responses.
+        alertDialogBuilder.setCancelable(false) // Makes the dialog non-cancelable, requiring a user response.
+        alertDialogBuilder.show()
+    }
+
+    private fun checkNumberInput(input: String): Boolean {
+        return input.all { it.isDigit() }
     }
 
     private fun showTimerFinishedAlert() {
